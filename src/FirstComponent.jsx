de0c2f7d997f61a29fba53bd29952a6deddfbaac
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-13 09:26:09 +0600

    first test component

---
+++ src/FirstComponent.jsx
@@ -0,0 +1,16 @@
+const FirstComponent = ()=>{
+
+    const name = "Phitron";
+
+    const sum = (a,b)=> a + b;
+
+    return (
+    <>
+        <h1> hello {name || "World"}</h1> 
+        <p>paragraph tag</p>
+        <p>Your marks is  {sum(5,34)}</p>
+    </>
+    );
+};
+
+export default FirstComponent;
