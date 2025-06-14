dfc2745caba6edb1e32f09d3e861f4334896ec46
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-14 20:44:46 +0600

    managing state

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,15 @@
+import { useState } from "react";
 
 const List = () => {
     const fruits = ['Apple','Banana','Orange','Grapes','Mango','Pineapple'];
-    let selectedIndex = -1;
-    const handleItemPrint = (index)=> {
-        selectedIndex = index;
-    };
-    
+    const [selectedIndex, setSelectedIndex]= useState(-1);
     return (
         <div>
             <ul className="pl-6 list-disc">
                 {fruits.map((fruit,index)=>(
-                    <li className={selectedIndex === index ? "bg-blue-500 p-3 rounded-sm m-3" : ""} onClick={()=>handleItemPrint(index)} key={fruit}>{fruit}</li>
+                    <li className={selectedIndex === index ? "bg-blue-500 p-3 rounded-sm m-3" : ""} 
+                    onClick={()=>setSelectedIndex(index)} 
+                    key={fruit}>{fruit}</li>
                 ))}
             </ul>
         </div>
