2a8e6470c26ea713d2a677b93496409b0077f9f5
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-14 22:22:55 +0600

    passing data

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import List from './components/list'
 import FirstComponent from './FirstComponent'
 
 function App() {
+  const fruits = ['Apple','Banana','Orange','Grapes','Mango','Pineapple'];
+  const cities = ['Dhaka','Khulna','Barisal','Sylhet','Chattogram','Rajshahi'];
   return (
     <>
       <FirstComponent></FirstComponent>
-      <List></List>
+      <List items={fruits} heading="Fruites"/>
+      <List items={cities} heading="Cities"/>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
-const List = () => {
-    const fruits = ['Apple','Banana','Orange','Grapes','Mango','Pineapple'];
+// eslint-disable-next-line react/prop-types
+const List = ({items=[], heading}) => {
     const [selectedIndex, setSelectedIndex]= useState(-1);
     return (
         <div>
+            <h1>{heading}</h1>
             <ul className="pl-6 list-disc">
-                {fruits.map((fruit,index)=>(
-                    <li className={selectedIndex === index ? "bg-blue-500 p-3 rounded-sm m-3" : ""} 
+                {items.map((fruit,index)=>(
+                    <li className={selectedIndex === index ? "bg-blue-500 p-3 rounded-sm" : ""}
                     onClick={()=>setSelectedIndex(index)} 
                     key={fruit}>{fruit}</li>
                 ))}
