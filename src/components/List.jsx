51c735e7ee8206a96fc7bdb1a916a4f83ee1cf6a
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-14 20:33:42 +0600

    handeling event

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,16 @@
 
 const List = () => {
     const fruits = ['Apple','Banana','Orange','Grapes','Mango','Pineapple'];
+    let selectedIndex = -1;
+    const handleItemPrint = (index)=> {
+        selectedIndex = index;
+    };
+    
     return (
         <div>
             <ul className="pl-6 list-disc">
-                {fruits.map((fruit)=>(
-                    <li key={fruit}>{fruit}</li>
+                {fruits.map((fruit,index)=>(
+                    <li className={selectedIndex === index ? "bg-blue-500 p-3 rounded-sm m-3" : ""} onClick={()=>handleItemPrint(index)} key={fruit}>{fruit}</li>
                 ))}
             </ul>
         </div>
