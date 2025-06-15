bf6260c363334c610422025ca40b3cc2b570f0fb
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-15 21:22:51 +0600

    reusable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 
+import Button from './components/Button';
 import List from './components/list'
 import FirstComponent from './FirstComponent'
 
 function App() {
   const fruits = ['Apple','Banana','Orange','Grapes','Mango','Pineapple'];
   const cities = ['Dhaka','Khulna','Barisal','Sylhet','Chattogram','Rajshahi'];
+  
+  const handleClick =()=> console.log('button');
   return (
     <>
       <FirstComponent></FirstComponent>
       <List items={fruits} heading="Fruites"/>
       <List items={cities} heading="Cities"/>
+      <Button handleClick={handleClick} color="info">My Button</Button>
     </>
   );
 };
---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+
+
+const Button = ({handleClick,children,color="info"}) => {
+
+    const btnColors = {
+        primary:'bg-blue-500',
+        success:'bg-green-500',
+        danger:'bg-red-500',
+        info:'bg-cyan-500',
+        warning:'bg-yellow-500'
+      };
+
+
+    return ( <button onClick={handleClick} 
+        className={`m-5 px-3 py-2 text-white rounded-sm ${btnColors[color]}`}>
+        {children}</button>
+    );
+};
+
+export default Button;
