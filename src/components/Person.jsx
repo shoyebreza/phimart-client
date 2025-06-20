c8dafa6e46c89db03bb85c6d3d48b1fff3fd3e2c
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-20 23:14:45 +0600

    update state

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,17 +2,34 @@ import { useState } from 'react';
 
 const Person = () => {
 
-    const [person, setPerson] = useState("");
-
-    const [visible, setVisible] = useState(false);
+    const [person, setPerson] = useState({
+        firstName : "John",
+        lastName : "Doe",
+        email : "example@email.com",
+        address: {
+            city: "Mirpur",
+            state : "Dhaka",
+        },
+    });
 
     const handleClick = ()=>{
-        setPerson('John');
-        console.log(person);
+        const newPerson = {
+            ... person, 
+            firstName : "Michel",
+            lastName : "Von",
+            address : {
+                ...person.address,
+                city : "Dhanmondi",
+            },
+        };
+
+        setPerson(newPerson);
+
     };
-    console.log(person);
     return (
         <div>
+            <h1>{person.firstName} {person.lastName}</h1>
+            <p>{person.address.city} {person.address.state}</p>
             <button onClick={handleClick} className='px-2 py-2 bg-blue-500 text-white rounded-sm m-5'>Click Me</button>
         </div>
     );
