7cc628bb1bd4f0e2e513687ef91ee5d8ea431097
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-26 23:13:58 +0600

    effect

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 let count =0;
 
 
 const Effect = () => {
+    const [users, setUsers]= useState([])
     useEffect(()=>{
         console.log('effect occur');
+        setUsers("john")
     });
+
+
     return (
         <div>
             <h1>User List {++count}</h1>
