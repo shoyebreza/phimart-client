400ece3ecb3dfdabff29e28791531bc99e540f53
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-02 22:50:52 +0600

    form validation

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -2,7 +2,7 @@
 import { useForm } from "react-hook-form";
 
 const HookForm = () => {
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, formState: {errors}} = useForm();
     const onSubmit = (data) => {
         console.log(data);
     }
@@ -12,13 +12,16 @@ const HookForm = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="m-2 p-2">
                     <label htmlFor="">Name :</label>
-                    <input { ...register('name')}
+                    <input { ...register('name', {required: true, minLength: 5})}
                     type="text" name="name" id="name"  className="px-3 py-3 border-gray-500"/>
+                    {errors.name?.type==='required' && <span>  This field is required</span> }
+                    {errors.name?.type==='minLength' && <span>  Minimun length is 5 </span> }
                 </div>
                 <div className="m-5 p-5">
                     <label htmlFor="">Age :</label>
-                    <input { ...register('age')}
+                    <input { ...register('age',{required: true})}
                     type="number" name="age" className="px-3 py-3 border-gray-500"/>
+                    {errors.age && <span>  This field is required</span> }
                 </div>
                 <button type="submit" className="bg-blue-500 text-white rounded-sm p-3">submit</button>
             </form>
