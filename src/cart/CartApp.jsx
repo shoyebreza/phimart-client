a85cb97c574244b163d81f84c3bb956e234416e4
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-04 18:23:27 +0600

    design updated

--- src/cart/CartApp.jsx
+++ src/cart/CartApp.jsx
@@ -1,3 +1,4 @@
+import { Trash2 } from "lucide-react";
 import { useForm } from "react-hook-form";
 
 
@@ -15,20 +16,31 @@ const CartApp = () => {
         console.log(data);
     };
     return (
-        <div>
+        <div className="w-1/3 mx-auto bg-gray-100 rounded-lg p-6 shadow-md">
             <form action="" onSubmit={handleSubmit(addItem)}>
-                <input {...register('item',{required:true})} type="text" placeholder="item Name" className="border p-2 m-2 rounded w-1/2"/>
-                <input {...register('price',{required:true})} type="number" placeholder="item Price" className="border p-2 m-2 rounded w-1/2"/>
+                <input {...register('item',{required:true})} type="text" placeholder="item Name" className="border p-2 m-2 rounded w-full"/>
+                <input {...register('price',{required:true})} type="number" placeholder="item Price" className="border p-2 m-2 rounded w-full"/>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded w-1/2 m-2">Add item</button>
             </form>
             {/* cart item list */} 
             {cart.length >0 ? (
                 //Todo Cart
-                <ul>
+                <div>
                     {cart.map((item, index)=>(
-                        <li key={index}>1 X {item.name} {item.price}</li>
+                        <div key={index} className="flex items-center p-2 bg-white rounded mb-2 justify-between">
+                            <p>{item.name}</p>
+                            <p>{item.price}</p>
+
+                            <div className="flex items-center">
+                                <button className="bg-red-500 text-white px-2 py-1 rounded">-</button>
+                                <span className="mx-2"> 1 </span>
+                                <button  className="bg-green-500 text-white px-2 py-1 rounded">+</button>
+                            </div>
+                            <button><Trash2/></button>
+                        </div>
                     ))}
-                </ul>
+                    <div className="mt-4 font-black text-large"> Total Price: 100 </div>
+                </div>
             ) : (
                 <p>No cart item</p>
             )}
