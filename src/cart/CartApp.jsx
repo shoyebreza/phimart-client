4b2f85436faa8c1f97d9369a937b41b5b57ca8d5
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-04 20:06:56 +0600

    add cart item

--- src/cart/CartApp.jsx
+++ src/cart/CartApp.jsx
@@ -1,24 +1,35 @@
 import { Trash2 } from "lucide-react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 
 
 const CartApp = () => {
     const {register, handleSubmit} = useForm();
+    const [cart, setCart] = useState([]);
 
-    const cart =[
+    /* const cart =[
         {name : "Laptop", price : 34000},
         {name : "Mobile", price : 14000},
         {name : "Desktop", price : 54000},
         {name : "Camera", price : 24000}
-    ];
+    ]; */
     const addItem = (data)=> {
-        console.log(data);
+        const existingItem = cart.find((item)=> item.name ===data.name);
+        if(existingItem){
+            setCart(cart.map(item => item.name===data.name ? {...item, quantity: item.quantity + 1} : item))
+        }else{
+        setCart([
+            ...cart, 
+            { name: data.name, price: parseFloat(data.price), 
+            quantity: 1},
+        ]);
+      }
     };
     return (
         <div className="w-1/3 mx-auto bg-gray-100 rounded-lg p-6 shadow-md">
             <form action="" onSubmit={handleSubmit(addItem)}>
-                <input {...register('item',{required:true})} type="text" placeholder="item Name" className="border p-2 m-2 rounded w-full"/>
+                <input {...register('name',{required:true})} type="text" placeholder="item Name" className="border p-2 m-2 rounded w-full"/>
                 <input {...register('price',{required:true})} type="number" placeholder="item Price" className="border p-2 m-2 rounded w-full"/>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded w-1/2 m-2">Add item</button>
             </form>
@@ -29,11 +40,11 @@ const CartApp = () => {
                     {cart.map((item, index)=>(
                         <div key={index} className="flex items-center p-2 bg-white rounded mb-2 justify-between">
                             <p>{item.name}</p>
-                            <p>{item.price}</p>
+                            <p>{(item.price * item.quantity).toFixed(2)}</p>
 
                             <div className="flex items-center">
                                 <button className="bg-red-500 text-white px-2 py-1 rounded">-</button>
-                                <span className="mx-2"> 1 </span>
+                                <span className="mx-2"> {item.quantity} </span>
                                 <button  className="bg-green-500 text-white px-2 py-1 rounded">+</button>
                             </div>
                             <button><Trash2/></button>
